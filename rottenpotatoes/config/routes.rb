518a403c69e60ec31256e253d0aca83c5340b17b
JiJi <ngohaanh764@gmail.com>
2016-05-09 02:17:43 +0000

    fixfeature

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
+  match 'movies/:id/same_director' => 'movies#same_director', :as => :same_director, :via => [:get]
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 end
